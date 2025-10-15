7f606b986d89f7bacfebeec09b9fe5d2031df9c8
SaraAlameen82 <saramohamed0802@gmail.com>
2025-10-15 16:39:48 +0300

    Adding The initial code for Assignment 3

---
+++ Assignment3/business.js
@@ -0,0 +1,181 @@
+const persistence = require('./persistence')
+
+/**
+ * Retrieves all albums from the "albums.json" file.
+ * 
+ * @returns {Promise<Array<Object>>} An array of album objects.
+ */
+async function getAlbums() {
+    return await persistence.getAlbums()
+}
+
+/**
+ * Retrieves all photos from the "photos.json" file.
+ * 
+ * @returns {Promise<Array<Object>>} An array of photo objects.
+ */
+async function getPhotos() {
+    return await persistence.getPhotos()
+}
+
+
+/**
+ * This function finds a photo by its ID.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to find
+ * @param {Array<Object>} photos - An array of photos to get the photo from.
+ * @returns {Promise<Object|undefined>} Returns a photo object if it is found, or undefined if the photo is not found.
+ */
+async function findPhoto(photoID) {
+    let photos = await persistence.getPhotos()
+    for (let p of photos) {
+        if (p.id === photoID) {
+            return p
+        }
+    }
+    return undefined
+}
+
+
+/**
+ * Retrieves the names of albums linked to a photo.
+ * 
+ * @async
+ * @param {Array<Number>} albumID - An array of album IDs linked to the photo.
+ * @returns {Promise<Array<String>>} An array of album names.
+ */
+async function getPhotoAlbums(albumID) {
+    let albums = await persistence.getAlbums()
+    let photoAlbums = []
+    // Looping through the albums to match their ID's to the ones linked to the photo.
+    for (let a of albums) {
+        if (albumID.includes(a.id)) {
+            photoAlbums.push(a.name)
+        }
+    }
+    return photoAlbums
+}
+
+
+/**
+ * Updates the title and description of a photo.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to update.
+ * @param {String} newDescription - The new description for the photo.
+ * @param {String} newTitle - The new title for the photo.
+ */
+async function updatePhotos(photoID, newDescription, newTitle) {
+    let photos = await getPhotos()
+    let photo = await findPhoto(photoID)
+
+    // Checking if the user gave an input or pressed enter.
+    if (newTitle.length > 0) {
+        photo.title = newTitle
+    }
+    if (newDescription.length > 0) {
+        photo.description = newDescription
+    }
+
+    // Finding the photo index
+    let index = -1
+    for (let i = 0; i < photos.length; i++) {
+        if (photos[i].id === photoID) {
+            index = i
+            break
+        }
+    }
+
+    // Updating the photos array 
+    if (index !== -1) {
+        photos[index] = photo
+    }
+
+    // Updating the 'photos.json' file
+    await persistence.updatePhotos(photos)
+}
+
+
+/**
+ * Finds all photos linked to a specified album and accessible by the user.
+ * 
+ * @async
+ * @param {String} albumName - The name of the album.
+ * @returns {Promise<Array<Object>|undefined>} An array of photo objects or undefined if the album is not found.
+ */
+async function findAlbumPhotos(albumName) {
+    let albums = await getAlbums()
+    let photos = await getPhotos()
+
+    // Looping through the albums to find the album by its name.
+    for (a of albums) {
+        if (a.name.toLowerCase() === albumName.toLowerCase()) {
+            let albumID = a.id
+            let photoList = []
+
+            // Looping through the photos to match them with the ones in the specified album.
+            for (p of photos) {
+                if (p.albums.includes(albumID)) {
+                    photoList.push(p)
+                }
+            }
+            return photoList
+        }
+    }
+    return undefined
+}
+
+
+/**
+ * Adds a tag to a photo's list of tags.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to add a tag to.
+ * @param {String} newTag - The tag to add to the photo.
+ * @returns {Promise<String>} "exists" if the tag already exists, "updated" if the tag is added successfully.
+ */
+async function addPhotoTag(photoID, newTag) {
+    let photos = await getPhotos()
+    let photo = await findPhoto(photoID)
+    let photoTags = photo.tags
+
+    // Checking if the tag already exists.
+    if (photoTags.includes(newTag)) {
+        return "exists"
+    }
+    else {
+        // Add the new tag
+        photo.tags.push(newTag)
+
+        // Find the index of the photo in the array
+        let index = -1
+        for (let i = 0; i < photos.length; i++) {
+            if (photos[i].id === photoID) {
+                index = i
+                break
+            }
+        }
+
+        // Update the photo in the array
+        if (index !== -1) {
+            photos[index] = photo
+        }
+
+        // Write the updated photos array to the file
+        await persistence.updatePhotos(photos)
+        return "updated"
+
+    }
+}
+
+
+module.exports = {
+    findPhoto,
+    getAlbums,
+    getPhotos,
+    getPhotoAlbums,
+    updatePhotos,
+    findAlbumPhotos,
+    addPhotoTag
+}
---
+++ Assignment3/presentation.js
@@ -0,0 +1,142 @@
+const prompt = require('prompt-sync')()
+const business = require('./business')
+
+
+/**
+ * Displays the details of a photo.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to display.
+ */
+async function displayPhoto(photoID) {
+    let photo = await business.findPhoto(photoID)
+
+    if (photo === undefined) {
+        // Printing an error message if the photo is not found.
+        console.log("\nPhoto not found.")
+    }
+    else {
+        let albumID = photo.albums
+        let photoAlbums = await business.getPhotoAlbums(albumID)
+
+        console.log(`\nFilename: ${photo.filename}`)
+        console.log(`Title: ${photo.title}`)
+        // Converting the date's format. 
+        console.log(`Date: ${new Date(photo.date).toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        })}`)
+        console.log(`Albums: ${photoAlbums}`)
+        console.log(`Tags: ${photo.tags}`)
+    }
+}
+
+
+/**
+ * Prompts the user to modify the title and description of a photo.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to update.
+ */
+async function updatePhotoDetails(photoID) {
+    let photo = await business.findPhoto(photoID)
+    if (photo === undefined) {
+        console.log("\nPhoto not found.")
+    }
+    else {
+        console.log('check---------')
+        console.log("Press Enter to reuse existing values.")
+        let newTitle = prompt(`Enter value for title [${photo.title}]: `)
+        let newDescription = prompt(`Enter value for description [${photo.description}]: `)
+        // Updating the file with the modifications.
+        await business.updatePhotos(photoID, newDescription, newTitle)
+        console.log("\nPhoto updated!")
+    }
+}
+
+
+/**
+ * Displays all photos linked to a specified album.
+ * 
+ * @async
+ * @param {String} albumName - The name of the album.
+ */
+async function albumPhotoList(albumName) {
+    let photoList = await business.findAlbumPhotos(albumName)
+    if (photoList === undefined) {
+        console.log(`\nThere is no album with the name "${albumName}"`)
+    }
+    else {
+        console.log("\nfilename      resolution      tags")
+        console.log('-----------------------------------------')
+        for (p of photoList) {
+            let tags = p.tags
+            console.log(`${p.filename}   -   ${p.resolution}   -   ${tags.join(':')}`)
+        }
+    }
+}
+
+
+/**
+ * Prompts the user to add a tag to a photo's list of tags.
+ * 
+ * @async
+ * @param {Number} photoID - The ID of the photo to add a tag to.
+ */
+async function addTag(photoID) {
+    let photo = await business.findPhoto(photoID)
+    if (photo === undefined) {
+        console.log("\nPhoto not found.")
+    }
+    else {
+        let newTag = prompt(`What tag would you like to add (${photo.tags})? `)
+        let result = await business.addPhotoTag(photoID, newTag)
+        // Make addPhotoTag return "exists" if the tag already exists, and "updated" if the tag is added successfully.
+        if (result === "exists") {
+            console.log(`Photo '${photoID}' already has the tag '${newTag}'`)
+        }
+        else {
+            console.log("\nUpdated!")
+        }
+    }
+}
+
+
+/**
+ * Main program loop that allows the user to login and perform different tasks.
+ * 
+ * @async
+ */
+async function main() {
+    while (true) {
+        console.log("\n1. Find Photo \n2. Update Photo Details \n3. Album Photo List \n4. Tag Photo \n5. Exit\n")
+        let choice = Number(prompt("Your Selection: "))
+
+        if (choice === 1) {
+            let photoID = Number(prompt("Photo ID? "))
+            await displayPhoto(photoID)
+        }
+        else if (choice === 2) {
+            let photoID = Number(prompt("Photo ID? "))
+            await updatePhotoDetails(photoID)
+        }
+        else if (choice === 3) {
+            let albumName = prompt("What is the name of the album? ")
+            await albumPhotoList(albumName)
+        }
+        else if (choice === 4) {
+            let photoID = Number(prompt("What photo ID to tag? "))
+            await addTag(photoID)
+        }
+        else if (choice === 5) {
+            process.exit()
+        }
+        else {
+            console.log("Try Again!!")
+        }
+    }
+}
+
+
+main()
